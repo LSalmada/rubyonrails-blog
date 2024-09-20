b7fa3d1eccbfdac920efbc43c425505facee7adf
LSalmada <lucas.almadag7@gmail.com>
2024-09-20 16:56:26 -0300

    Aula 83

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,5 +4,16 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.includes(:category, :author, comments: :user ).friendly.find(params[:id])
     @other_articles = Article.all.sample(3)
+    @comments = comments_sorted
+  end
+
+  private
+
+  def comments_sorted
+    puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    puts params[:sort_by]
+    return @article.comments.order(created_at: :desc) if params[:sort_by] == "most_recent"
+
+    @article.comments.order(created_at: :asc)
   end
 end
